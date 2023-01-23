6078153dca0534543f0df22bb8622d28eeac8b95
Nizar Sbai <nizar1.sbai@gmail.com>
2023-01-23 17:10:30 +0000

    Initial commit

---
+++ src/main/java/com/nizarinfo/demospring_cloudstreams_kafka/entities/PageEvent.java
@@ -0,0 +1,14 @@
+package com.nizarinfo.demospring_cloudstreams_kafka.entities;
+
+
+import lombok.*;
+
+import java.util.Date;
+
+@Data @AllArgsConstructor @NoArgsConstructor @ToString
+public class PageEvent {
+        private String name;
+        private String user;
+        private Date date;
+        private long duration;  // en ms
+}
